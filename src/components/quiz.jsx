33fa29fa1688939be0100e3ce13d699512805d39
barkiya <sahilpanchal824824@gmail.com>
2024-08-28 16:59:02 +0530

    completed

--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -2,12 +2,47 @@ import React from 'react'
 import "./quiz.css"
 import data from "../assets/data.js"
 import { useState } from 'react'
+import { useRef } from 'react'
 
 const Quiz = () => {
    
     const [currentIndex,setcurrentIndex]=useState(0);
     const [question , setquestion]=useState(data[currentIndex]);
     const [lock , setlock] = useState(false);
+    const [result,setresult] = useState(false);
+    const [score,setscore] = useState(0);
+
+
+    let option1=useRef(null);
+    let option2=useRef(null);
+    let option3=useRef(null);
+    let option4=useRef(null);
+
+
+
+    let option_array=[option1,option2,option3,option4];
+
+    function reset(){
+        setcurrentIndex(0);
+        setquestion(data[currentIndex]);
+        setlock(false);
+        setresult(!result)
+}
+
+    function next(){ 
+         
+        if(currentIndex===data.length-1){
+            setresult(!result)
+        }
+       
+       else{ setcurrentIndex((currentIndex)=>currentIndex+1)
+        setquestion(data[currentIndex]) 
+        setlock(!lock)
+        option_array.map((option)=>{
+            option.current.classList.remove("correct");
+            option.current.classList.remove("wrong");
+        })}
+    }
 
     function checkAnswer(e,ans){
   if(!lock)
@@ -15,31 +50,41 @@ const Quiz = () => {
                    
                  e.target.classList.add("correct");
                  setlock(true);
+                 setscore(score+1);
           }
 
           else{
             e.target.classList.add("wrong")
             setlock(true);
+            option_array[question.ans-1].current.classList.add("correct");
           }
         
         }
     }
   return (
-    <div className='quiz'>
-        <h2>QUIZ-APP</h2>
-        <hr />
-        <p>Q.{currentIndex+1} {data[currentIndex].question}</p>
-        <ul>
-            <li onClick={(e)=>checkAnswer(e,1)}>{data[currentIndex].option1}</li>
-            <li onClick={(e)=>checkAnswer(e,2)}>{data[currentIndex].option2}</li>
-            <li onClick={(e)=>checkAnswer(e,3)}>{data[currentIndex].option3}</li>
-            <li onClick={(e)=>checkAnswer(e,4)}>{data[currentIndex].option4}</li>
-        </ul>
+    <>{result===false?<div className='quiz'>
+    <h2>QUIZ-APP</h2>
+    <hr />
+    <p>Q.{currentIndex+1} {data[currentIndex].question}</p>
+    <ul>
+        <li ref={option1} onClick={(e)=>checkAnswer(e,1)}>{data[currentIndex].option1}</li>
+        <li ref={option2} onClick={(e)=>checkAnswer(e,2)}>{data[currentIndex].option2}</li>
+        <li ref={option3} onClick={(e)=>checkAnswer(e,3)}>{data[currentIndex].option3}</li>
+        <li ref={option4} onClick={(e)=>checkAnswer(e,4)}>{data[currentIndex].option4}</li>
+    </ul>
 
-        <button onClick={()=> setcurrentIndex((currentIndex)=>currentIndex+1)}>next</button>
-        <p id="para">Question {currentIndex+1} out of 5</p>
+    <button onClick={()=>{next()} }>next</button>
+    <p id="para">Question {currentIndex+1} out of {data.length}</p>
 
-    </div>
+</div>:
+<div className='quiz'>
+<h2>QUIZ-APP</h2>
+<hr />
+<p>You Scored {score} out of {data.length}</p>
+<button onClick={()=>{reset()}}>Reset</button>
+</div>
+}</>
+
   )
 }
 
